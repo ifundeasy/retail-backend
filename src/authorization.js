13556be93adb32229554050c3d79624ad6be2b30
Afa Rappresent <mail.vudin@gmail.com>
2018-01-02 02:09:04 +0700

    fix authorization user data loader

--- src/authorization.js
+++ src/authorization.js
@@ -5,35 +5,65 @@ module.exports = function (compile) {
         let {user} = req.logged;
         let roleData = await compile(`
             SELECT 
-                a.id actor_id, a.name actor_name,
-                b.api_id, c.name api_name, b.apiRoute_id, d.name apiRoute_name,
-                d.http_id, e.code http_code, e.name http_name
+                a.id actor_id, a.name actor_name, b.module_id,
+                c.module_id module_parent, c.name module_name, 
+                c.class module_class, c.seq module_seq, c.notes module_notes,
+                d.TABLENAME, d.value route, d.httpmethod_id, 
+                e.name httpmethod_name, e.code httpmethod_code
             FROM personActor z
-            LEFT JOIN actor a ON z.actor_id = a.id AND z.status_id = 1
-            LEFT JOIN actorRoute b ON z.actor_id = b.actor_id AND b.status_id = 1
-            LEFT JOIN api c ON b.api_id = c.id AND c.status_id = 1
-            LEFT JOIN apiRoute d ON b.apiRoute_id = d.id AND d.status_id = 1
-            LEFT JOIN http e ON d.http_id = e.id AND e.status_id = 1
-            WHERE z.id = 1 AND z.status_id = ?
-            ORDER BY c.id, d.id, e.id
+            LEFT JOIN actor a ON z.actor_id = a.id AND z.op_id = 1
+            LEFT JOIN actorModule b ON a.id = b.actor_id AND b.op_id = 1
+            LEFT JOIN module c ON c.id = b.module_id AND c.op_id = 1
+            LEFT JOIN moduleRoute d ON d.module_id = c.id AND d.op_id = 1
+            LEFT JOIN httpmethod e ON e.id = d.httpmethod_id AND e.op_id = 1
+            WHERE z.id = ? AND z.op_id = 1
         `, user.id);
-        if (roleData instanceof Error) throw actor;
+        if (roleData instanceof Error) throw roleData;
 
         let actor = { id: roleData[0].actor_id, name: roleData[0].actor_name };
-        let authorized = {};
-        roleData.forEach(function (o) {
-            let { api_id, api_name, apiRoute_id, apiRoute_name, http_code } = o;
+        let routes = {};
+        let modules = {};
+        for (let a in roleData) {
+            let role = roleData[a];
+            let {
+                module_id, module_parent, module_name,
+                module_class, module_seq, module_notes
+            } = role;
 
-            authorized[apiRoute_name] = authorized[apiRoute_name] || {};
-            authorized[apiRoute_name].apiRoute_id = apiRoute_id;
-            authorized[apiRoute_name].apiRoute_name = apiRoute_name;
-            authorized[apiRoute_name].page = api_name;
-            authorized[apiRoute_name].page_id = api_id;
-            authorized[apiRoute_name].methods = authorized[apiRoute_name].methods || [];
-            authorized[apiRoute_name].methods.push(http_code)
-        });
+            if (role.route) {
+                routes[role.route] = routes[role.route] || {value: role.route, table: role.TABLENAME};
+                routes[role.route].methods = routes[role.route].methods || [];
+                routes[role.route].methods.push(role.httpmethod_code)
+            }
+            if (!modules.hasOwnProperty(module_id)) {
+                modules[module_id] = {
+                    id: module_id,
+                    parent: module_parent,
+                    name: module_name,
+                    class: module_class,
+                    seq: module_seq,
+                    notes: module_notes
+                }
+            }
+        }
+        /** For nested data modules **/
+        //for (let b in modules) {
+        //    let node = modules[b];
+        //    if (node.parent) {
+        //        let parent = modules[node.parent];
+        //        parent.children = modules[node.parent].children || [];
+        //        parent.children.push(node);
+        //    }
+        //}
+        //for (let c in modules) {
+        //    if (modules[c].parent) delete modules[c];
+        //}
+        //modules = Object.keys(modules).map(function (k) {
+        //    return modules[k]
+        //});
         req.logged.actor = actor;
-        req.logged.authorized = authorized;
+        req.logged.routes = routes;
+        req.logged.modules = modules;
         next();
     }
 };
