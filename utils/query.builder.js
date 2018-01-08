18df463b118f132ad7e47157fac04f7c485bb317
Afa Rappresent <mail.vudin@gmail.com>
2018-01-08 17:55:34 +0700

    fix wrong regex

--- utils/query.builder.js
+++ utils/query.builder.js
@@ -8,22 +8,22 @@ moSQL.raw = function ({query, values}) {
     //    if (val.constructor == String) return `"${val}"`;
     //    return val;
     //})
-	let q = query.replace(/\"/g, '').replace(/\@/g, '.').replace(/\`\"/g);
-	return q.replace(/\$[0-9]/g, function(e){
-		let val = values[e.substr(1)-1];
-		if (val) {
-			if (val.constructor == String) {
-				val = `"${val}"`.replace(/\"\`|\`\"/g, '`')
-			}
-		} else if (val == '') {
-			return '""'
-		}
-		return val;
-	})
+    let q = query.replace(/\"/g, '').replace(/\@/g, '.').replace(/\`\"/g);
+    return q.replace(/\$[0-9]+/g, function (e) {
+        let val = values[e.substr(1) - 1];
+        if (val) {
+            if (val.constructor == String) {
+                val = `"${val}"`.replace(/\"\`|\`\"/g, '`')
+            }
+        } else if (val == '') {
+            return '""'
+        }
+        return val;
+    })
 };
-module.exports = function (query) {
-	var res = moSQL.sql(query);
-	res.raw = moSQL.raw(res);
-	res.stringy = sqlFormatter.format(res.raw);
-	return res;
+module.exports = function (opts) {
+    let res = moSQL.sql(opts);
+    res.raw = moSQL.raw(res);
+    res.stringy = sqlFormatter.format(res.raw);
+    return res;
 };
