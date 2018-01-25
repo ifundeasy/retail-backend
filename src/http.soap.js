03db2fd38808d3830473e4f3322a57d0f8c6694e
Afa Rappresent <mail.vudin@gmail.com>
2018-01-26 01:43:35 +0700

    add get product soap, this load product information with taxes, discounts, and product tag information

--- src/http.soap.js
+++ src/http.soap.js
@@ -1,9 +1,7 @@
 const _temp = {};
 const fs = require('fs');
-const Promise = require('bluebird');
 const {Router} = require('express');
 const route = Router();
-const avoidFields = ['salt', 'password'];
 const jsonParse = function (string) {
     try {
         throw JSON.parse(string);
@@ -11,181 +9,36 @@ const jsonParse = function (string) {
         return e
     }
 };
-const describeColumns = async function (name) {
-    let columns = await _temp.compileFn(`DESCRIBE \`${name}\``);
-    return columns.map(function (row) {
-        return row.Field;
-    });
-};
-const getRelation = async function (name, mode) {
-    let up = [], down = [];
-    let relation = await _temp.compileFn(`
-        SELECT
-          IF (TABLE_NAME = '${name}', 0, 1) IS_CHILD,
-          TABLE_NAME 'TABLE',
-          COLUMN_NAME 'COLUMN',
-          REFERENCED_TABLE_NAME TABLE_REF,
-          REFERENCED_COLUMN_NAME COLUMN_REF
-        FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
-        WHERE
-          COLUMN_NAME != 'id' AND 
-          REFERENCED_TABLE_SCHEMA = '${_temp.database}' AND 
-          TABLE_SCHEMA = '${_temp.database}' AND
-          (TABLE_NAME = '${name}' OR REFERENCED_TABLE_NAME = '${name}')
-        ORDER BY IF (TABLE_NAME = '${name}', 0, 1);
-    `);
-    relation.forEach(function (row) {
-        let o = {
-            table: row.TABLE,
-            column: row.COLUMN,
-            table_ref: row.TABLE_REF,
-            column_ref: row.COLUMN_REF
-        };
-        if (row.IS_CHILD) down.push(o);
-        else up.push(o);
-    });
-    return !mode ? {up, down} : mode === 'up' ? up : down;
-};
-const getParentRelations = async function (name, alias) {
-    let parents = await getRelation(name, 'up');
-    let mapper = [name].concat(parents);
-    let columns = [], joins = [];
+const getAdtQuery = function (object, query = {}) {
+    let filter, sorter, error = [], {table} = object;
 
-    await Promise.map(mapper, async function (relate, i) {
-        let join = {};
-        let table_name = i ? relate.table_ref : name;
-        let cols = await describeColumns(table_name);
-        let newAlias = relate.column ? relate.column.substr(0, relate.column.length - relate.column_ref.length - 1) : '';
-
-        if (i) {
-            join = {
-                target: table_name,
-                alias: 'r' + i,
-                on: {[relate.column_ref]: `$z.${relate.column}$`}
-            };
-
-            if (table_name !== 'op') join.on['op_id'] = 1;
-            if (table_name === name) join.type = 'left';
-            joins.push(join);
-        }
-
-        cols.forEach(function (col) {
-            let columnName = [join.alias || alias, col].join('.');
-            let asColumn = i ? [newAlias, col].join('_') : join.alias ? columnName : col;
-            let isExist = columns.filter(function (col) {
-                let avoid = avoidFields.filter(function (field) {
-                    if (asColumn.indexOf(field) > -1) return 0;
-                    return 1;
-                });
-                let added = avoid.length === avoidFields.length;
-                if ((col.as === asColumn) || !added) return 1;
-                return 0;
-            });
-            if (!isExist.length) {
-                columns.push({
-                    name: columnName,
-                    as: asColumn
-                });
-            }
-        })
-    });
-
-    return {columns, joins, parents}
-};
-const setQuery = async function (object, method, query = {}, body = {}) {
-    let filter, error = [], {table} = object,
-        columns = await describeColumns(object.table);
-
-    if (['POST', 'PUT'].indexOf(method) > -1 && !Object.keys(body).length) {
-        error.push('body payloads value');
-    }
-    if (query.hasOwnProperty('filter') && method !== 'POST') {
+    if (query.hasOwnProperty('filter')) {
         filter = jsonParse(query.filter);
         if (!filter) error.push(filter.message);
         if (!Object.keys(filter).length) error.push('query string for filter value');
     }
-    if (error.length) return new Error(`Invalid ${error.join(',')}`);
-    //
-    if (method === 'POST') {
-        body = body.constructor === Object ? [body] : body;
-        object.type = 'insert';
-        object.values = body.map(function (row) {
-            let data = {};
-
-            columns.forEach(function (field) {
-                if (row[field]) data[field] = row[field];
-            });
-            data.op_id = 1;
-
-            return data
-        });
-    } else if (method === 'PUT') {
-        object.operations = [];
-        body = body.constructor === Object ? [body] : body;
-        body.forEach(function (row) {
-            let data = {};
-            let condition = row._;
-            let {id} = condition;
-            columns.forEach(function (field) {
-                if (row.hasOwnProperty(field)) {
-                    if (((field.slice(-3) === '_id') && !row[field]) || !row[field]) data[field] = null;
-                    else data[field] = row[field]
+    if (query.hasOwnProperty('sort')) {
+        sorter = jsonParse(query.sort);
+        if (!sorter) error.push(sorter.message);
+        if (sorter.length) {
+            object.order = {};
+            sorter.forEach(function (sort = {}, i) {
+                let {property, direction} = sort;
+                if (property && direction) {
+                    object.order[property] = direction.toLowerCase();
+                } else {
+                    error.push(`query string for sort value at index-${i} require "property" and "direction" key`);
                 }
             });
-            data.op_id = 1;
-
-            if (condition.hasOwnProperty('id') && id) {
-                object.operations.push({
-                    table: table,
-                    type: 'update',
-                    updates: data,
-                    where: {id}
-                })
-            } else {
-                object.operations.push({
-                    table: table,
-                    type: 'insert',
-                    values: data
-                })
-            }
-        });
-    } else if (method === 'DELETE') {
-        body = body.constructor === Object ? [body] : body;
-        object.type = 'update';
-        let $in = body.filter(function (o) {
-            if (o.id) return 1;
-            return 0;
-        }).map(function (o) {
-            return o.id
-        });
-        object.updates = {op_id: 2};
-        object.where = {id: {$in}};
-    } else if (method === 'GET') {
-        let parents;
-
-        filter = filter || {};
-        filter.op_id = 1;
-        object.alias = 'z';
-        object.type = 'select';
-        object.where = filter;
-        object.offset = parseInt(query.offset) || 0;
-        object.limit = parseInt(query.limit) || 100;
-        try {
-            let sorter = JSON.parse(query.sort);
-            if (sorter.length) {
-                object.order = {};
-                sorter.forEach(function (sort) {
-                    let {property, direction} = sort;
-                    object.order[property] = direction.toLowerCase();
-                });
-            }
-        } catch (e) {
-            //
         }
-        parents = await getParentRelations(table, object.alias);
-        object.columns = parents.columns;
-        object.joins = parents.joins;
     }
+    if (error.length) return new Error(`Invalid ${error.join(',')}`);
+    //
+    filter = filter || {};
+    object.type = 'select';
+    object.where = filter;
+    object.offset = parseInt(query.offset) || 0;
+    object.limit = parseInt(query.limit) || 100;
     return object
 };
 //
@@ -194,10 +47,9 @@ module.exports = function ({Glob, locals, compile}) {
     const qbuilder = require(`${Glob.home}/utils/query.builder`);
     const {name} = locals;
     const authorizing = async function (req, res, next) {
-        let {method, query, body} = req,
+        let {method} = req,
             {routes} = req.logged,
             name = req._parsedUrl.pathname.substr(1),
-            {status, message} = httpCode.OK,
             reqUrl = '/soap/' + name;
 
         try {
@@ -208,9 +60,6 @@ module.exports = function ({Glob, locals, compile}) {
             //if (routes[reqUrl].methods.indexOf(method) === -1) {
             //    throw new Error(`You can't access ${reqUrl} route with ${method}'s method`);
             //}
-            //
-            req.queryObj = await setQuery({table: name}, method, query, body);
-            if (req.queryObj instanceof Error) throw req.queryObj;
             return next();
         } catch (e) {
             return next(e);
@@ -219,43 +68,130 @@ module.exports = function ({Glob, locals, compile}) {
     _temp.database = Glob.config.mysql.database;
     _temp.compileFn = compile;
     //
-    route.get('/product', authorizing, function (req, res, next) {
+    route.get('/product', authorizing, async function (req, res, next) {
+        let {query} = req;
         let {status, message} = httpCode.OK;
-        let {method, query, body, queryObj} = req;
-        let nestedSelection = {
-            type:  'select',
-            table: 'state',
-            alias: 'z',
-            joins: [
-                {
-                    type: 'left',
-                    target: {
-                        type: 'select',
-                        alias: 'regency',
-                        table: {
-                            type: 'select',
-                            table: 'regency',
-                            where: {
-                                op_id: 1,
-                                name: {
-                                    $like : '%art%'
-                                }
-                            },
-                            order: {
-                                name: 'DESC'
-                            }
-                        },
-                        groupBy: ['state_id']
-                    },
-                    alias: 'r1',
-                    on: {
-                        id: '$z.status_id$',
-                        op_id: 1
+        try {
+            let adtQuery = qbuilder(getAdtQuery({table: 'WTF'}, query)).raw;
+            let productQuery = `
+                SELECT 
+                    z.id, z.name,
+                    r3.id productCode_id,
+                    r3.code productCode_code,
+                    z.brand_id, r2.name brand_name,
+                    r4.id productPrice_id, r4.price productPrice_price,
+                    r4.type_id, r4.type_name,
+                    r4.unit_id, r4.unit_name, r4.unit_shortname,
+                    z.status_id, r6.name status_name,
+                    z.notes,
+                    z.product_id, r1.name product_name
+                FROM product z
+                LEFT JOIN product r1 ON r1.id = z.product_id AND r1.op_id = 1
+                LEFT JOIN brand r2 ON r2.id = z.brand_id AND r2.op_id = 1
+                LEFT JOIN (
+                    SELECT * 
+                    FROM (SELECT * FROM productCode WHERE op_id = 1 ORDER BY id DESC) a 
+                    GROUP BY product_id
+                ) r3 ON r3.product_id = z.id
+                LEFT JOIN (
+                    SELECT b.*, \`unit\`.name unit_name, \`unit\`.short unit_shortname, \`type\`.name type_name
+                    FROM (SELECT * FROM productPrice WHERE op_id = 1 ORDER BY id DESC) b
+                    JOIN \`type\` ON \`type\`.id = b.type_id AND \`type\`.op_id = 1
+                    JOIN \`unit\` ON \`unit\`.id = b.unit_id AND \`unit\`.op_id = 1
+                    GROUP BY product_id, b.type_id
+                ) r4 ON r4.product_id = z.id
+                LEFT JOIN \`status\` r6 ON r6.id = z.status_id AND r6.op_id = 1
+                WHERE (z.op_id = 1)
+            `;
+            let tags = [], discounts = [], taxes = [], pricesIds = [], productIds = [];
+            let total, products = await compile(`
+                SELECT * FROM (${productQuery}) product
+                ${adtQuery.substr(adtQuery.indexOf('where')).replace(/WTF\./g, '')}
+            `);
+            if (products.length) {
+                productIds = products.map(function (product) {
+                    let priceId = product.productPrice_id;
+                    if (pricesIds.indexOf(priceId) < 0) {
+                        pricesIds.push(product.productPrice_id);
                     }
+                    return product.id
+                });
+                tags = await compile(`
+                    SELECT
+                        a.id, a.product_id,
+                        a.tag_id, b.name tag_name,  
+                        b.tag_id tag_tag_id, c.name tag_tag_name,
+                        a.status_id, a.notes
+                    FROM productTag a 
+                    LEFT JOIN tag b ON b.id = a.tag_id AND b.op_id = 1
+                    LEFT JOIN tag c ON c.id = b.tag_id AND c.op_id = 1
+                    WHERE a.product_id in (${productIds.join(',')}) AND a.op_id = 1
+                `);
+                if (tags.length) {
+                    tags.forEach(function(tag){
+                        products.forEach(function (product) {
+                            product.productTag = product.productTag || [];
+                            if (product.id === tag.product_id) {
+                                product.productTag.push(tag);
+                            }
+                        });
+                    })
                 }
-            ]
-        };
-        res.send(qbuilder(nestedSelection))
+            }
+            if (pricesIds.length) {
+                discounts = await compile(`
+                    SELECT
+                        a.id, a.productPrice_id,
+                        a.discount_id,
+                        b.name discount_name, 
+                        b.isPercent discount_isPercent,
+                        b.value discount_value,
+                        b.notes discount_notes,
+                        a.status_id, a.notes
+                    FROM productPriceDisc a 
+                    LEFT JOIN discount b ON b.id = a.discount_id AND b.op_id = 1
+                    WHERE a.productPrice_id in (${pricesIds.join(',')}) AND a.op_id = 1
+                `);
+                taxes = await compile(`
+                    SELECT
+                        a.id, a.productPrice_id,
+                        a.tax_id,
+                        b.name tax_name, 
+                        b.isPercent tax_isPercent,
+                        b.value tax_value,
+                        b.notes tax_notes,
+                        a.status_id, a.notes
+                    FROM productPriceTax a 
+                    LEFT JOIN tax b ON b.id = a.tax_id AND b.op_id = 1
+                    WHERE a.productPrice_id in (${pricesIds.join(',')}) AND a.op_id = 1
+                `);
+                if (discounts.length) {
+                    pricesIds.forEach(function (priceId) {
+                        let productPriceDisc = discounts.filter(function (discount) {
+                            if (discount.productPrice_id === priceId) return 1;
+                            return 0;
+                        });
+                        let productPriceTax = taxes.filter(function (discount) {
+                            if (discount.productPrice_id === priceId) return 1;
+                            return 0;
+                        });
+                        products.forEach(function (product) {
+                            if (product.productPrice_id === priceId) {
+                                product.productPriceDisc = productPriceDisc;
+                                product.productPriceTax = productPriceTax
+                            }
+                        });
+                    })
+                }
+            }
+            total = await compile(`SELECT COUNT(*) xy FROM (${productQuery}) product`);
+            res.send({status, message, total: total[0].xy, data: products});
+        } catch (e) {
+            if (e.message.indexOf('Unexpected token') === 0) {
+                let error = 'Invalid query string for filter value, it should be JSON format. ' + e.message;
+                next(new Error(error));
+            } else next(e);
+        }
     });
     return route;
 };
